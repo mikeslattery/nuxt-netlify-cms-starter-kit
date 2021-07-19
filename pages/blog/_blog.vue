6b0e877f8bc65af3cfa79f9f384f8fcda4640c7b
Michael Slattery <m2slattery@gmail.com>
2021-07-19 17:43:47 -0400

    payload.body

--- pages/blog/_blog.vue
+++ pages/blog/_blog.vue
@@ -49,7 +49,7 @@ export default {
     };
   },
   async asyncData({ params, payload }) {
-    if (payload) return { blogPost: payload };
+    if (payload && payload.body) return { blogPost: payload };
     else
       return {
         blogPost: await require(`~/assets/content/blog/${params.blog}.json`),
